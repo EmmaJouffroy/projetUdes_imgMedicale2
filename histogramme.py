da7e36a029412f8f1808ca2e6df97b3e137c92df
madolphe <madolphe@ensc.fr>
2019-10-30 18:28:35 -0400

    init histo + pipfile ok

--- histogramme.py
+++ histogramme.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from PIL import Image
 
 def JointHist(I, J, bin):
     """
@@ -13,4 +13,11 @@ def JointHist(I, J, bin):
     # On trouve d'abbord la plage de valeurs conjointes
     intensiteI_min = I.amin()
     intensiteJ_min = J.amin()
+
+
+if __name__ == '__main__':
+    I = np.array(Image.open('Data/I1.png'))
+    print(type(I))
+    plt.imshow(I)
+    plt.show()
 
