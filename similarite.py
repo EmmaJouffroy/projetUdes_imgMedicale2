5ca90a3191813851478e642687f21672f9d4456c
madolphe <madolphe@ensc.fr>
2019-10-31 01:17:34 -0400

    develop all methods but one bug remains

--- similarite.py
+++ similarite.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+from histogramme import jointHist
 
 def SSD(I, J):
     """
@@ -18,7 +19,13 @@ def CR(I, J):
     :param J:
     :return:
     """
-    return
+    mean_I = np.mean(I)
+    mean_J = np.mean(J)
+    centered_I = I - mean_I
+    centered_J = J - mean_J
+    num = np.sum(centered_I*centered_J)
+    denom = np.sqrt(np.sum(centered_I**2))*np.sqrt(np.sum(centered_J**2))
+    return num / denom
 
 def IM(I, J):
     """
@@ -27,10 +34,18 @@ def IM(I, J):
     :param J:
     :return:
     """
-    return
+    hist = jointHist(I, J, bin=100)
+    # Pour avoir les histogramme de I et J on utilise la même fonction et on ne considérera que la diagonale:
+    histJ = jointHist(J, J, bin=100)
+    histI = jointHist(I, I, bin=100)
+    IM = 0
+    for i in range(100):
+        for j in range(100):
+            IM += hist[i, j] * (np.log2(hist[i, j]/(histI[i, i]*histJ[j, j])))
+    return IM
 
 
 if __name__ == '__main__':
     I = np.array(Image.open('Data/I2.jpg'))
     J = np.array(Image.open('Data/J2.jpg'))
-    print(SSD(I,J))
+    print(IM(I, J))
