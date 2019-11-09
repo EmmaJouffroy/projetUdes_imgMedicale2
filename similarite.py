e366a9b22e1fbfbd236d7129a444ce6ceb578ab3
madolphe <madolphe@ensc.fr>
2019-11-09 15:43:46 -0500

    Similarite add to menu

--- similarite.py
+++ similarite.py
@@ -50,7 +50,7 @@ def IM(hist):
 
 def afficher_similarites(images_I,images_J, ssd, cr, im ):
     """
-
+    Fonction permettant l'affichage des différentes images à comparer
     :param images_I:
     :param images_J:
     :param ssd:
@@ -61,73 +61,65 @@ def afficher_similarites(images_I,images_J, ssd, cr, im ):
     ax1 = plt.subplot2grid((2, 6), (0, 0), colspan=1)
     ax1.axis('off')
     ax1.imshow(images_I[0])
-
     ax2 = plt.subplot2grid((2, 6), (1, 0), colspan=1)
     ax2.axis('off')
     ax2.imshow(images_J[0])
-
     ax6 = plt.subplot2grid((2, 6), (0, 1), colspan=1)
     ax6.axis('off')
     ax6.imshow(images_I[1])
-
     ax7 = plt.subplot2grid((2, 6), (1, 1), colspan=1)
     ax7.axis('off')
     ax7.imshow(images_J[1])
-
     ax11 = plt.subplot2grid((2, 6), (0, 2), colspan=1)
     ax11.axis('off')
     ax11.imshow(images_I[2])
-
     ax12 = plt.subplot2grid((2, 6), (1, 2), colspan=1)
     ax12.axis('off')
     ax12.imshow(images_J[2])
-
     ax16 = plt.subplot2grid((2, 6), (0, 3), colspan=1)
     ax16.axis('off')
     ax16.imshow(images_I[3])
-
     ax17 = plt.subplot2grid((2, 6), (1, 3), colspan=1)
     ax17.axis('off')
     ax17.imshow(images_J[3])
-
     ax21 = plt.subplot2grid((2, 6), (0, 4), colspan=1)
     ax21.axis('off')
     ax21.imshow(images_I[4])
-
     ax22 = plt.subplot2grid((2, 6), (1, 4), colspan=1)
     ax22.axis('off')
     ax22.imshow(images_J[4])
-
     ax26 = plt.subplot2grid((2, 6), (0, 5), colspan=1)
     ax26.axis('off')
     ax26.imshow(images_I[5])
-
     ax27 = plt.subplot2grid((2, 6), (1, 5), colspan=1)
     ax27.axis('off')
     ax27.imshow(images_J[5])
-
     plt.show()
 
 
+def test_afficher_similarites():
+    images_I = [np.array(Image.open('Data/I1.png')), np.array(Image.open('Data/I2.jpg')),
+                np.array(Image.open('Data/I3.jpg')),  np.array(Image.open('Data/I4.jpg')),
+                np.array(Image.open('Data/I5.jpg')), np.array(Image.open('Data/I6.jpg'))]
+
+    images_J = [np.array(Image.open('Data/J1.png')), np.array(Image.open('Data/J2.jpg')),
+                np.array(Image.open('Data/J3.jpg')), np.array(Image.open('Data/J4.jpg')),
+                np.array(Image.open('Data/J5.jpg')), np.array(Image.open('Data/J6.jpg'))]
+    ssd = []
+    cr = []
+    im = []
+
+    for i in range(0, 6):
+        ssd.append(SSD(images_I[i], images_J[i]))
+        cr.append(CR(images_I[i], images_J[i]))
+        im.append(IM(jointHist(images_I[i], images_J[i], bin=20)))
+
+    afficher_similarites(images_I, images_J, ssd, cr, im)
+    print("SSD=", ssd)
+    print("Coefficient de corrélation=", cr)
+    print("Information mutuelle=", im)
+
+
 if __name__ == '__main__':
-
-    def test_afficher_similarites():
-        images_I = [np.array(Image.open('Data/I1.png')), np.array(Image.open('Data/I2.jpg')),
-                    np.array(Image.open('Data/I3.jpg')),  np.array(Image.open('Data/I4.jpg')),
-                    np.array(Image.open('Data/I5.jpg')), np.array(Image.open('Data/I6.jpg'))]
-
-        images_J = [np.array(Image.open('Data/J1.png')), np.array(Image.open('Data/J2.jpg')),
-                    np.array(Image.open('Data/J3.jpg')), np.array(Image.open('Data/J4.jpg')),
-                    np.array(Image.open('Data/J5.jpg')), np.array(Image.open('Data/J6.jpg'))]
-        ssd = []
-        cr = []
-        im = []
-
-        for i in range(0, 6):
-            ssd.append(SSD(images_I[i], images_J[i]))
-            cr.append(CR(images_I[i], images_J[i]))
-            im.append(IM(jointHist(images_I[i], images_J[i], bin=20)))
-
-        afficher_similarites(images_I, images_J, ssd, cr, im)
-
-    test_afficher_similarites()
+    pass
+    # test_afficher_similarites()
--- tp2.py
+++ tp2.py
@@ -1,6 +1,6 @@
 from histogramme import test_afficher_histogramme, test_joint_histogramme
 from transformation import *
-from similarite import *
+from similarite import test_afficher_similarites
 from transformation_interpolations import *
 import sys
 
@@ -20,10 +20,13 @@ if __name__ == '__main__':
                 print("Pour lancer la question 1, suivre l'usage suivant:")
                 print("python main.py 1 NumeroDuCouple")
                 print("Les couples sont indexes de 1 à 6")
-    elif sys.argv[1] == 2:
+    elif int(sys.argv[1]) == 2:
         # lancer les tests des fonctions de similarité
-        print('allo')
+        print("Patientez, les images comparées s'afficheront et les résulats des différentes mesures de similarité "
+              "seront affichés dans la console.")
+        test_afficher_similarites()
     elif sys.argv[1] == 3:
+        # lancer l'affichage d'une transformation
         print('allo')
     elif sys.argv[1] == 4:
         print('allo')
